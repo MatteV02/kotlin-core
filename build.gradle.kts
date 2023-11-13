9b0f301a950a01a09ff1158cdde4e1abf6bd0fe5
MatteV02 <veronimatteo1@gmail.com>
2023-11-13 13:05:02 +0100

    Add Kotest dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,16 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    // https://mvnrepository.com/artifact/io.kotest/kotest-runner-junit5-jvm
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.8.0")
+    // https://mvnrepository.com/artifact/io.kotest/kotest-assertions-core-jvm
+    testImplementation("io.kotest:kotest-assertions-core-jvm:5.8.0")
+    // https://mvnrepository.com/artifact/io.kotest/kotest-property-jvm
+    implementation("io.kotest:kotest-property-jvm:5.8.0")
+    // https://mvnrepository.com/artifact/io.kotest/kotest-framework-datatest-jvm
+    implementation("io.kotest:kotest-framework-datatest-jvm:5.8.0")
+
+
 }
 
 tasks.test {
